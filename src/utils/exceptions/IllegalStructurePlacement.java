cce91085b9310d8ad817b62ac21e920649882db5
Jakub Maciejewski <jakubm55555@gmail.com>
2021-05-25 11:14:26 +0200

    Dodanie Okna Dialogowego, przy wyjątku o niepoprawnym ułożeniu struktury

--- src/utils/exceptions/IllegalStructurePlacement.java
+++ src/utils/exceptions/IllegalStructurePlacement.java
@@ -3,6 +3,7 @@ package utils.exceptions;
 public class IllegalStructurePlacement extends Exception{
     @Override
     public String getMessage() {
+        ExceptionsDialogs.warningDialog("Warning", "Tried illegal placement of structure");
         return "Tried illegal placement structure in CellMap";
     }
 }
